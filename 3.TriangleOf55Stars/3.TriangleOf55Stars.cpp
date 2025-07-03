145c658b4d88d8b9d3ef714b857d58f6d76327b4
Miroslava Novikova <diwaina2@yandex.ru>
2025-07-03 20:44:22 +0700

    Задание 3: треугольник из 55 звёзд

---
+++ 3.TriangleOf55Stars/3.TriangleOf55Stars.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+int main() {
+
+	int n = 11;
+
+	for (int r = 1; r <= n; ++r) {
+		for (int c = 1; c <= r; ++c) std::cout << '*';
+		std::cout << '\n';
+	}
+
+	std::cin.get(); std::cin.get();
+	return 0;
+}
